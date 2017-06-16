7e2caf1bc512d66cee3d5a865e1534f13e032031
Shaisan2 <shivam0sharma@icloud.com>
2017-06-16 14:20:28 -0400

    Added Characters to scroll view

--- Walking Dead Store/ViewController.swift
+++ Walking Dead Store/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let WIDTH: CGFloat = 271
+    let HEIGHT: CGFloat = 428
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for x in 1...5 {
+            let image = UIImage(named: "\(x)")
+            let imageView = UIImageView(image: image)
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: -WIDTH + (WIDTH * CGFloat(x)), y: 320, width: WIDTH, height: HEIGHT)
+            
+            scrollView.contentSize = CGSize(width: WIDTH * 5, height: scrollView.frame.size.height)
+        }
     }
 
 }
